f6bb36a8c2264980a9af6f0bb8bc1d2b020143c8
ebatuhan <120257510+ebatuhan@users.noreply.github.com>
2022-12-10 18:41:21 +0300

    ready

--- Recursion/Recursion.cpp
+++ Recursion/Recursion.cpp
@@ -1,20 +1,158 @@
-// Recursion.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+#include <fstream>
 #include <iostream>
+#include <iomanip>
+#include <string>
+using namespace std;
+const string fileName("inputs.txt");
+const int len = 100;
+
+
+bool isDot(char*& arr);
+bool isSign(char*& arr);
+bool isE(char*& arr);
+bool isDigit(char*& arr);
+bool isUInt(char*& arr, bool flag);
+bool isOrdered(char*& arr);
+bool isMantis(char*& arr);
+bool isExpression(char*& arr);
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	ifstream ifs;
+
+	ifs.open(fileName);
+
+	try
+	{
+		if (!ifs)
+		{
+			throw fileName + " file not found.";
+		}
+
+		while (!ifs.eof())
+		{
+			char* str;
+			str = new char[len];
+			ifs.getline(str, len, '\n');
+			char* x = str;
+			cout << str;
+			if (isExpression(x))
+			{
+				cout << " True\n";
+			}
+			else
+			{
+				cout << " False\n";
+			}
+		}
+	}
+
+	catch (const string& error)
+	{
+		cerr << "\nError:" << error << "\n";
+		exit(-1);
+	}
+	return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+bool isDot(char*& arr)
+{
+	if (*arr == '.')
+	{
+		arr++;
+		return true;
+	}
+	return false;
+}
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+bool isSign(char*& arr)
+{
+	if (*arr == '+' || *arr == '-')
+	{
+		arr++;
+		return true;
+	}
+	return false;
+}
+
+bool isE(char*& arr)
+{
+	if (*arr == 'E')
+	{
+		arr++;
+		return true;
+	}
+	return false;
+}
+
+bool isDigit(char*& arr)
+{
+	if (*arr >= '0' && *arr <= '9')
+	{
+		arr++;
+		return true;
+	}
+	return false;
+}
+
+bool isUInt(char*& arr, bool flag)
+{
+	if (isDigit(arr))
+	{
+		if ((*arr == '.' && flag == true) || *arr == '\0' || *arr == 'E')
+		{
+			return true;
+		}
+		return isUInt(arr, flag);
+	}
+	return false;
+}
+
+bool isOrdered(char*& arr)
+{
+	if (isE(arr))
+	{
+		if (isSign(arr))
+		{
+			if (isUInt(arr, false))
+			{
+				return true;
+			}
+		}
+	}
+	return false;
+
+}
+
+bool isMantis(char*& arr)
+{
+	if (isUInt(arr, true) || *arr == '.')
+	{
+		if (isDot(arr))
+		{
+			if (isUInt(arr, false))
+			{
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
+bool isExpression(char*& arr)
+{
+	if (isSign(arr))
+	{
+		if (isMantis(arr))
+		{
+			if (isOrdered(arr))
+			{
+				return true;
+			}
+		}
+	}
+
+	return false;
+}
+
